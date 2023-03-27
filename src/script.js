17abef1cfddfbe0e87dd137c0197f26ca0447c9d
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2023-03-27 00:39:46 -0300

    atualizado o html e js

--- src/script.js
+++ src/script.js
@@ -5,6 +5,7 @@ fetch(url)
   .then(response => response.json())
   .then(data => {
     const lista = document.getElementById('mylist');
+    document.createElement('ul');
     data.forEach(item => {
       const li = document.createElement('li');
     li.innerHTML = `${item.category} <span class="values-status"> ${item.score} <span class="values-status-valor"> / 100</span></span>`;
