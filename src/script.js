e7f495927f089a22f308fa8b4eb3a77c01b0f169
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2023-03-27 10:14:45 -0300

    css e js atualizado

--- src/script.js
+++ src/script.js
@@ -9,7 +9,15 @@ fetch(url)
     lista.appendChild(ul);
     data.forEach(item => {
       const li = document.createElement('li');
-    li.innerHTML = `${item.category} <span class="values-status"> ${item.score} <span class="values-status-valor"> / 100</span></span>`;
+    li.innerHTML = 
+    `<div class="categoria">     
+      <img src="${item.icon}" alt="icon">
+      ${item.category}
+    </div>    
+    <div> 
+      <span class="values-status"> ${item.score} </span>
+      <span class="values-status-valor"> / 100</span>
+    </div>`;
       ul.appendChild(li);
     });
   });
